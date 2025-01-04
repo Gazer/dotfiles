c139c6901aff78a0f123900083bf44dcd1a19052
Ricardo Markiewicz <ricardo.markiewicz@contentsquare.com>
2025-01-04 02:42:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Things to install:
+
+```
+brew install neovim
+brew install ripgrep
+brew install tmux
+brew tap arl/arl
+brew install gitmun
+brew install television
+brew install tmuxinaton
+brew install fzf
+```
+
---
+++ nvim/lua/gazer/lazy/local.lua
@@ -0,0 +1,84 @@
+
+local local_plugins = {
+    { "nvim-neotest/nvim-nio" },
+
+    --{
+    --    "ThePrimeagen/harpoon",
+   --     branch = "harpoon2",
+   --     -- dir = "~/personal/harpoon",
+   --     config = function()
+   --         local harpoon = require("harpoon")
+
+   --         harpoon:setup()
+
+   --         vim.keymap.set("n", "<leader>A", function() harpoon:list():prepend() end)
+   --         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+   --         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+   --         vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+   --         vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+   --         vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+   --         vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+   --         vim.keymap.set("n", "<leader><C-h>", function() harpoon:list():replace_at(1) end)
+   --         vim.keymap.set("n", "<leader><C-t>", function() harpoon:list():replace_at(2) end)
+   --         vim.keymap.set("n", "<leader><C-n>", function() harpoon:list():replace_at(3) end)
+   --         vim.keymap.set("n", "<leader><C-s>", function() harpoon:list():replace_at(4) end)
+   --     end
+   -- },
+    {
+        'akinsho/flutter-tools.nvim',
+        lazy = false,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'stevearc/dressing.nvim', -- optional for vim.ui.select
+        },
+        config = function()
+            require("flutter-tools").setup({
+                decorations = {
+                    statusline = {
+                        device = false
+                    }
+                },
+            })
+
+            vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end);
+            vim.keymap.set("x", "<leader>ca", function() vim.lsp.buf.range_code_action() end);
+            require("telescope").load_extension("flutter")
+        end,
+    },
+    {
+        "vim-apm", dir = "~/personal/vim-apm",
+        config = function()
+            --[[
+            local apm = require("vim-apm")
+
+            apm:setup({})
+            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
+            --]]
+        end
+    },
+    {
+        "vim-with-me", dir = "~/personal/vim-with-me",
+        config = function() end
+    },
+    {
+     'stevearc/conform.nvim',
+        lazy = false,
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    dart = { "dart_format" }
+                }
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*.dart",
+                callback = function(args)
+                    require("conform").format({ bufnr = args.buf })
+                end,
+            })
+        end
+    },
+}
+
+return local_plugins
+
